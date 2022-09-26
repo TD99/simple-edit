5b4a3dc67e4bfaebf07779f7e4e37daee06d2e11
tim <tdu135585@stud.gibb.ch>
2022-09-26 11:43:07 +0200

    hello

---
+++ dev/v6/simple-edit/src/interface/js/script.js
@@ -0,0 +1,31 @@
+const { ipcRenderer } = require('electron');
+const ipc = ipcRenderer;
+
+function appControlEvt(event){
+    switch (event){
+        case 'minimize':
+            ipc.send('minimizeApp');
+            break;
+        case 'maximize':
+            ipc.send('maximizeApp');
+            break;
+        case 'close':
+            ipc.send('closeApp');
+            break;
+        default:
+            return false;
+    }
+
+}
+
+$(".minimizeButton").click(()=>{
+    appControlEvt("minimize");
+});
+
+$(".maximizeButton").click(()=>{
+    appControlEvt("maximize");
+});
+
+$(".closeButton").click(()=>{
+    appControlEvt("close");
+});
